cf9533be17d286d272326a34e4bd568cf93958b2
Akash <angadiakash2006@gmail.com>
2026-09-22 15:22:11 +0530

    Update home page message for automated deployment test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World! Automatic End-to-End CI/CD is fully working!", 200
+    return "Hello World! Automatic End-to-End CI/CD is fully work!", 200
 
 
 if __name__ == "__main__":
